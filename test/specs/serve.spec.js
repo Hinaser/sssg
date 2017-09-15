1b37bc7f1b84a766e2f5617d6e36ebd3987d1a4f
Hinaser <rindo.hinase@gmail.com>
2017-09-15 14:46:21 +0900

    Add test specs to server task

--- test/specs/serve.spec.js
+++ test/specs/serve.spec.js
@@ -2,8 +2,10 @@
 
 var chai = require('chai');
 var expect = chai.expect;
-var chaiHttp = require('chai-http');
-chai.use(chaiHttp);
+chai.use(require('chai-http'));
+var chaiFiles = require('chai-files');
+chai.use(chaiFiles);
+var file = chaiFiles.file;
 
 var Share = require('./shared');
 
@@ -23,17 +25,88 @@ describe('Serve', function() {
       });
     });
     
-    it("should return index.html", function(done){
-      this.timeout(10000);
+    describe('Request to "http://localhost:3000/"', function(){
+      var response = null;
       
-      chai.request("http://localhost:3000")
-        .get("/")
-        .end(function(err, res){
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          expect(res).to.be.html;
-          done();
-        })
-    })
+      before(function(done){
+        this.timeout(10000);
+  
+        chai.request("http://localhost:3000")
+          .get("/")
+          .end(function(err, res){
+            response = res;
+            done();
+          });
+      });
+      
+      it("should response status code 200", function(){
+        expect(response).to.have.status(200);
+      });
+      
+      it("should be html", function(){
+        expect(response).to.be.html;
+      });
+      
+      it("should be equal to local index.html file", function(){
+        expect(response.text).to.equal(file(__dirname + "/../testdata/input/dst/index.html"));
+      });
+    });
+    
+    describe('Request to "http://localhost:3000/css/main.css"', function(){
+      var response = null;
+    
+      before(function(done){
+        this.timeout(10000);
+      
+        chai.request("http://localhost:3000")
+          .get("/css/main.css")
+          .end(function(err, res){
+            response = res;
+            done();
+          });
+      });
+    
+      it("should response status code 200", function(){
+        expect(response).to.have.status(200);
+      });
+    
+      it("should be css", function(){
+        expect(response).to.have.header('content-type', /^text\/css/);
+      });
+    
+      it("should be equal to local main.css file", function(){
+        expect(response.text).to.equal(file(__dirname + "/../testdata/input/dst/css/main.css"));
+      });
+    });
+  
+    describe('Request to "http://localhost:3000/js/main.js"', function(){
+      var response = null;
+    
+      before(function(done){
+        this.timeout(10000);
+      
+        chai.request("http://localhost:3000")
+          .get("/js/main.js")
+          .end(function(err, res){
+            response = res;
+            done();
+          });
+      });
+    
+      it("should response status code 200", function(){
+        expect(response).to.have.status(200);
+      });
+    
+      it("should be javascript", function(){
+        expect(response).to.have.header('content-type', /^application\/javascript/);
+      });
+    
+      /*
+      // TODO Comment here out if chai-http can get data if content-type is application/javascript
+      it("should be equal to local main.js file", function(){
+        expect(response.text).to.equal(file(__dirname + "/../testdata/input/dst/js/main.js"));
+      });
+      */
+    });
   })
 });
