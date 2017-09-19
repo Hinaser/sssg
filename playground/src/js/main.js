3b644481be1502f9eb689bc8572b88ec6ba69fc7
Hinaser <rindo.hinase@gmail.com>
2017-09-19 19:14:18 +0900

    Update docs

--- playground/src/js/main.js
+++ playground/src/js/main.js
@@ -1,3 +1,25 @@
-import {demo_func1} from './sub1.part';
+// global $, jQuery
 
-console.log(demo_func1("Hello"));
+import {
+  describeInstall,
+  describeInit,
+  describeDevelop
+} from './sub1.part';
+
+$(() => {
+  const btn_install = $("#install");
+  const btn_init = $("#init");
+  const btn_dev = $("#develop");
+  
+  btn_install.on("click", (e) => {
+    describeInstall(e.target);
+  });
+  
+  btn_init.on("click", (e) => {
+    describeInit(e.target);
+  });
+  
+  btn_dev.on("click", (e) => {
+    describeDevelop(e.target);
+  });
+});
