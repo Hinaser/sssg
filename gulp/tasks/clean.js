74b3ce5ec0a32e906bbce49d2945c47de52aefc4
Hinaser <rindo.hinase@gmail.com>
2017-09-21 14:36:36 +0900

    Fix a bug in clean task

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -2,106 +2,86 @@ var gulp = require('gulp');
 var del = require('del');
 var fs = require('fs');
 
-/**
- * Check if directory is OK to delete.
- *
- * @returns {boolean} - true if it may be valid directory, false if it should not be deleted.
- */
-function isValidDir(){
-    var config = require('../config');
-
-    var indexFile = config['html']['destIndexDir'];
-    var htmlDir = config['html']['destDir'];
-    var cssDir = config['css']['destDir'];
-    var jsDir = config['js']['destDir'];
-
-    return fs.existsSync(indexFile)
-        && fs.existsSync(htmlDir)
-        && fs.existsSync(cssDir)
-        && fs.existsSync(jsDir);
-}
-
 gulp.task('clean:css', function(){
   var config = require('../config');
+  var destDir = config['css']['destDir'];
   
-  if(!isValidDir()){
+  if(!fs.existsSync(destDir)){
     return;
   }
   
-  var destDir = config['css']['destDir'];
-
   return del([destDir], {force: true});
 });
 
 gulp.task('clean:js', function(){
   var config = require('../config');
+  var destDir = config['js']['destDir'];
   
-  if(!isValidDir()){
+  if(!fs.existsSync(destDir)){
     return;
   }
   
-  var destDir = config['js']['destDir'];
-
     return del([destDir], {force: true});
 });
 
 gulp.task('clean:image', function(){
   var config = require('../config');
+  var destDir = config['image']['destDir'];
   
-  if(!isValidDir()){
+  if(!fs.existsSync(destDir)){
     return;
   }
   
-  var destDir = config['image']['destDir'];
-
   return del([destDir], {force: true});
 });
 
 gulp.task('clean:html', function(){
   var config = require('../config');
+  var destIndexFile = config['html']['destIndexDir'] + '/index.html';
+  var destDir = config['html']['destDir'];
+  var deletings = [];
   
-  if(!isValidDir()){
-    return;
+  if(fs.existsSync(destIndexFile)){
+    deletings.push(destIndexFile);
+  }
+  if(fs.existsSync(destDir)){
+    deletings.push(destDir);
   }
   
-  var destIndexDir = config['html']['destIndexDir'];
-  var destDir = config['html']['destDir'];
+  if(deletings.length < 1) return;
 
-  return del([destIndexDir + '/index.html', destDir], {force: true});
+  return del(deletings, {force: true});
 });
 
 gulp.task('clean:lib:js', function(){
   var config = require('../config');
+  var destFile = config['js']['destDir'] + '/lib.js';
   
-  if(!isValidDir()){
+  if(!fs.existsSync(destFile)){
     return;
   }
   
-  var destDir = config['js']['destDir'];
-
-  return del([destDir + '/lib.js'], {force: true});
+  return del([destFile], {force: true});
 });
 
 gulp.task('clean:lib:css', function(){
   var config = require('../config');
+  var destFile = config['css']['destDir'] + '/lib.css';
   
-  if(!isValidDir()){
+  if(!fs.existsSync(destFile)){
     return;
   }
   
-  var destDir = config['css']['destDir'];
-
-  return del([destDir + '/lib.css'], {force: true});
+  return del([destFile], {force: true});
 });
 
 gulp.task('clean:lib:misc', function(){
   var config = require('../config');
+  var destDir = config['misc']['destDir'];
   
-  if(!isValidDir()){
+  if(!fs.existsSync(destDir)){
     return;
   }
-  
-  var destDir = config['misc']['destDir'];
   
   return del([destDir], {force: true});
 });
