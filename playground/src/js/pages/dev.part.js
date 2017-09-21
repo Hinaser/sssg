37775ef1cdf5b78005f51de59c10620280f496af
Hinaser <rindo.hinase@gmail.com>
2017-09-21 18:50:22 +0900

    Add playground pages

--- playground/src/js/pages/dev.part.js
+++ playground/src/js/pages/dev.part.js
@@ -13,19 +13,38 @@ export default class DevController extends AppController {
   constructor(){
     super();
     
-    const sidebarLogo = $(".sidebar-logo > img");
-    const sidebarMenu = $(".sidebar-menu ul.nav > li");
+    this.sidebarLogo = $(".sidebar-logo > img");
+    this.sidebarMenu = $(".sidebar-menu ul.nav > li");
+    this.sidebarToggle = $(".sidebar-toggle");
+    this.mainDiv = $("#main");
     
+    this.setMenuAnime();
+    this.setMenuToggleEvent();
+    
+    this.sidebarLogo.on("click", (e) => {
+      window.location = "../../";
+    });
+    
+    $("#content-area").niceScroll();
+  }
+  
+  setMenuAnime(){
     const handleMenuEvent = (page, cssClass) => {
-      const menu = sidebarMenu.find(`a[data-page='${page}']`);
-      menu.on("mouseenter", (e) => sidebarLogo.addClass(cssClass));
-      menu.on("mouseleave", (e) => sidebarLogo.removeClass(cssClass));
+      const menu = this.sidebarMenu.find(`a[data-page='${page}']`);
+      menu.on("mouseenter", (e) => this.sidebarLogo.addClass(cssClass));
+      menu.on("mouseleave", (e) => this.sidebarLogo.removeClass(cssClass));
     };
-    
+  
     handleMenuEvent("overview", "rotate72");
     handleMenuEvent("code_html", "rotate144");
     handleMenuEvent("code_style", "rotate216");
     handleMenuEvent("code_js", "rotate288");
     handleMenuEvent("misc", "rotate");
+  }
+  
+  setMenuToggleEvent(){
+    this.sidebarToggle.on("click", (e) => {
+      $("#sidebar-area").toggleClass("expand");
+    });
   }
 }
