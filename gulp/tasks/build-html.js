3e9abdbd0ed91eab08a606249b5278e022ecd852
Hinaser <rindo.hinase@gmail.com>
2017-09-29 23:05:24 +0900

    Fix an issue where building html generates an invalid file

--- gulp/tasks/build-html.js
+++ gulp/tasks/build-html.js
@@ -11,31 +11,32 @@ var runSequence = require('run-sequence');
 var gutil = require('gulp-util');
 var path = require('path');
 
-var debug = require('gulp-debug');
-
 /**
  * Build html file from source pug files.
  * This task builds not only html but also css/js/image files.
  */
-gulp.task('build:html', function(){
+gulp.task('build:html', function(cb){
   var startTime = new Date().getTime();
   
   return runSequence(['build:html:root', 'build:html:sub'], function(){
     gutil.log("build:html finished in: " + (new Date().getTime() - startTime) + "ms");
+    cb();
   });
 });
 
 gulp.task("build:html:root", function(){
   var config = require('../config.js');
+  var indexFilter = filter(function(file){
+    return path.resolve(config['html']['srcDir'] + "/index.pug") === file.path;
+  });
   
   return gulp.src([config['html']['srcDir'] + "/**/*.pug"], {base: config['html']['srcDir']})
     .pipe(plumber())
+    .pipe(gulpif(!global.isWatching, indexFilter))
     .pipe(changed(config['html']['destIndexDir'], {extension: '.html'}))
     .pipe(gulpif(global.isWatching, cached('pug')))
     .pipe(pugInheritance({basedir: path.resolve(config['html']['srcDir']), skip: 'node_modules'}))
-    .pipe(filter(function(file){
-      return path.resolve(config['html']['srcDir'] + "/index.pug") === file.path;
-    }))
+    .pipe(indexFilter)
     .pipe(pug({
       pretty: config['html']['pretty']
     }))
@@ -56,11 +57,15 @@ gulp.task("build:html:sub", function(){
     "!" + config['html']['srcDir'] + "/index.pug"
   ], {base: config['html']['srcDir']})
     .pipe(plumber())
+    .pipe(gulpif(global.isWatching, filter(function(file){
+      return !/\.part\.pug$/.test(file.relative);
+    })))
     .pipe(changed(config['html']['destDir'], {extension: '.html'}))
     .pipe(gulpif(global.isWatching, cached('pug')))
     .pipe(pugInheritance({basedir: path.resolve(config['html']['srcDir']), skip: 'node_modules'}))
     .pipe(filter(function(file){
-      return !/^.*\.part\.pug/.test(file.relative);
+      return !/\.part\.pug$/.test(file.relative)
+        && path.resolve(config['html']['srcDir'] + "/index.pug") !== file.path;
     }))
     .pipe(pug({
       pretty: config['html']['pretty']
